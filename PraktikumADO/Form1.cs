7803ea86e3b60ee50297da54dc9755a5a049cd45
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2026-03-12 12:51:31 +0630

    Mengatur alamat database ke dbakademikado dan menjalankan fungsi awal form

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -9,6 +9,15 @@ namespace PraktikumADO
         MySql.Data.MySqlClient.MySqlConnection conn;
         MySql.Data.MySqlClient.MySqlCommand cmd;
 
+        private void koneksi()
+        {
+            conn = new MySql.Data.MySqlClient.MySqlConnection("Server=127.0.0.1; database=dbakademikado; UID=root; Password=;");
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
        
     }
 }
