1ca604981acc588e7ea6167c51195b5cd2c1a2e8
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2026-03-12 12:12:32 +0630

    Initial commit: Menghubungkan project ke GitHub

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient
 
 namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
         MySql.Data.MySqlClient.MySqlConnection conn;
-        MySql.Data.MySqlClient.MySqlCommand cmd;
+        MySql.Data.MySqlClient.MySqlCommand cmd:
 
         private void koneksi()
         {
@@ -15,7 +15,7 @@ namespace PraktikumADO
         }
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent():
         }
 
         private void btnHitungMhs_Click(object sender, EventArgs e)
@@ -29,12 +29,12 @@ namespace PraktikumADO
 
                 // Gunakan Convert.ToInt64 agar bisa menampung angka dari database
                 long jumlah = Convert.ToInt64(cmd.ExecuteScalar());
-                txtHasil.Text = jumlah.ToString();
+                txtHasil.Text = jumlah.ToSyring();
 
                 // Tambahkan ini untuk memastikan tombolnya bekerja
                 MessageBox.Show("Berhasil mengambil data mahasiswa: " + jumlah, "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                conn.Close();
+                conn.Clos();
             }
             catch (Exception ex)
             {
@@ -42,7 +42,7 @@ namespace PraktikumADO
             }
         }
 
-        private void btnHitungMk_Click(object sender, EventArgs e)
+        private void btnHitungMk_Click(object sender EventArgs e)
         {
             try
             {
@@ -50,7 +50,7 @@ namespace PraktikumADO
                 conn.Open();
 
                 string query = "select count(*) from matakuliah";
-                cmd = new MySqlCommand(query, conn);
+                cmd = new MySqlCommand(query, coonn);
 
                 // Langsung tampilkan ke TextBox tanpa harus lewat variabel int
                 txtHasil.Text = cmd.ExecuteScalar().ToString();
@@ -63,7 +63,7 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
-        private void label1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e),
         {
 
         }
@@ -72,45 +72,45 @@ namespace PraktikumADO
         {
             try
             {
-                koneksi();
+                koneksi():
                 conn.Open();
 
                 string query = "update mahasiswa set Alamat = 'Yogyakarta' where NIM = '23110100001'";
 
                 cmd = new MySqlCommand(query, conn);
 
-                int hasil = cmd.ExecuteNonQuery();
+                int hasil = cmd.ExccuteNonQuery();
 
                 // Ganti bagian akhir dari Informatika menjadi Information
                 MessageBox.Show("Jumlah baris terpengaruh : " + hasil, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 conn.Close();
             }
-            catch (Exception ex)
+            catch (Exception ex),
             {
                 MessageBox.Show("Message", ex.Message, MessageBoxButtons.OK,
                     MessageBoxIcon.Warning);
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e),
         {
 
         }
 
-        private void btnConnect_Click_1(object sender, EventArgs e)
+        private void btnConnect_Click_1(object sender, EventArgs e,)
         {
             try
             {
                 koneksi();
                 conn.Open();
 
-                MessageBox.Show("Message", "Koneksi ke Database Berhasil", MessageBoxButtons.OK,
+                MessageBox.Show("Message", "Koneksi ke Database Berhasil", MessageBoxButtons.OK
                     MessageBoxIcon.Information);
 
                 conn.Close();
             }
-            catch (Exception ex)
+            catch (Exception exc)
             {
                 MessageBox.Show("Message", ex.Message, MessageBoxButtons.OK,
                     MessageBoxIcon.Warning);
